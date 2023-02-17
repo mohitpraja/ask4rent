cd81ed2f7f6566265a2f4f3249780b0c9f91ab4b
mohitpraja <immohitpraja@gmail.com>
2023-02-17 10:03:59 +0530

    launcher icon added

--- lib/core/allpages.dart
+++ lib/core/allpages.dart
@@ -1,6 +1,6 @@
 import 'package:ask4rent/core/routes.dart';
-import 'package:ask4rent/feature/splash/splash_bind.dart';
-import 'package:ask4rent/feature/splash/splash_view.dart';
+import 'package:ask4rent/feature/splash/binding/splash_bind.dart';
+import 'package:ask4rent/feature/splash/view/splash_view.dart';
 import 'package:get/get.dart';
 
 List<GetPage> allpages = [
--- lib/feature/splash/binding/splash_bind.dart
+++ lib/feature/splash/binding/splash_bind.dart
@@ -1,4 +1,5 @@
-import 'package:ask4rent/feature/splash/splash_cont.dart';
+
+import 'package:ask4rent/feature/splash/controller/splash_cont.dart';
 import 'package:get/get.dart';
 
 class SplashBinding extends Bindings{
