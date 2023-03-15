21f47290b0398a397502da173ef76f7f5d18589d
mohitpraja <immohitpraja@gmail.com>
2023-03-15 21:33:32 +0530

    splash controller added

--- lib/feature/splash/controller/splash_cont.dart
+++ lib/feature/splash/controller/splash_cont.dart
@@ -5,12 +5,22 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 
 class SplashController extends GetxController {
+  bool isLogin = false;
   @override
-  Future<void> onInit() async {
-    var db = await Hive.openBox('ask4rent');
-    bool isLogin = db.get('isLogin');
-    Timer(const Duration(seconds: 3),
-        () => isLogin ? Routes.dashboard : Routes.login);
+  void onInit() {
     super.onInit();
+    getdata();
+    Timer(const Duration(seconds: 3), () {
+      if (isLogin) {
+          Get.offAllNamed(Routes.dashboard);
+        
+      } else {
+        Get.offAllNamed(Routes.login);
+      }
+    });
+  }
+   Future<void> getdata() async {
+    var db = await Hive.openBox('ubivisit');
+    isLogin = db.get('isLogin')!;
   }
 }
