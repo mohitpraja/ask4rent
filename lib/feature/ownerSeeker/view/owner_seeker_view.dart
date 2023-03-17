08a3b478201bfb5e490736a0add433dabf448744
ubitechfeature <shivakushwah760@gmail.com>
2023-03-17 15:59:27 +0530

    ownerSeeker pageUI done by shiva

---
+++ lib/feature/ownerSeeker/view/owner_seeker_view.dart
@@ -0,0 +1,41 @@
+import 'package:ask4rent/core/global/colors.dart';
+import 'package:ask4rent/feature/ownerSeeker/controller/owner_seeker_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OwnerSeekerView extends GetView<OwnerSeekerController> {
+  const OwnerSeekerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: controller.tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          titleSpacing: 0,
+          leading: InkWell(
+              onTap: () {
+                Get.back();
+              },
+              child: Icon(Icons.arrow_back, color: white, size: 25)),
+          title: const Text('Dashboard'),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: Material(
+              color: Colors.grey.shade200,
+              child: SizedBox(
+                height: 55,
+                child: TabBar(
+                  physics: const BouncingScrollPhysics(),
+                  indicatorColor: primaryColor,
+                  tabs: controller.tabs,
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(children: controller.tabPages),
+      ),
+    );
+  }
+}
--- lib/feature/property/view/property_page.dart
+++ lib/feature/property/view/property_page.dart
@@ -20,7 +20,7 @@ class PropertyPage extends GetView<PropertyController> {
             onPressed: () {
               Get.back();
             },
-            icon: const Icon(Icons.arrow_back, color: Colors.grey, size: 30)),
+            icon: Icon(Icons.arrow_back, color: Colors.grey, size: 30)),
         backgroundColor: Colors.white,
         centerTitle: false,
         elevation: 0,
@@ -61,13 +61,14 @@ class PropertyPage extends GetView<PropertyController> {
               const SizedBox(
                 width: 5,
               ),
-              InkWell(onTap: () {
-
-              }, child:  FaIcon(
-                FontAwesomeIcons.chevronDown,
-                size: Get.width * 0.045,
-                color: Colors.black54,
-              ),),
+              InkWell(
+                onTap: () {},
+                child: FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Get.width * 0.045,
+                  color: Colors.black54,
+                ),
+              ),
               const SizedBox(
                 width: 10,
               ),
@@ -84,170 +85,191 @@ class PropertyPage extends GetView<PropertyController> {
               onTap: () {},
             ),
           ),
+          const SizedBox(
+            height: 5,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Text(
                 'Sort by',
-                style: AppStyle.appCityGrey,
+                style: AppStyle.sortBy,
               ),
               const SizedBox(
                 width: 5,
               ),
-              FaIcon(
-                FontAwesomeIcons.chevronDown,
-                size: Get.width * 0.045,
-                color: Colors.black54,
+              InkWell(
+                onTap: () {},
+                child: FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Get.width * 0.045,
+                  color: Colors.black54,
+                ),
               ),
-              SizedBox(
-                width: Get.width * 0.03,
-              )
+              const SizedBox(
+                width: 10,
+              ),
             ],
           ),
-          SizedBox(
-            height: Get.width * 0.05,
+          const SizedBox(
+            height: 5,
           ),
           Expanded(
-              child: Stack(
-            children: [
-              StreamBuilder(
-                builder:
-                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: Get.width * 0.95,
-                            height: Get.height * 0.6,
-                            decoration: BoxDecoration(
-                                color: Colors.grey[100],
-                                borderRadius: BorderRadius.circular(25)),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 20),
-                                  child: Image(
-                                    image: const AssetImage(
-                                        "assets/images/rent-house-1.jpg"),
-                                    width: Get.width * 0.9,
-                                    height: Get.width * 0.6,
+            child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                padding: EdgeInsets.only(bottom: 3),
+                itemCount: controller.houseImage.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Stack(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: Get.width * 0.95,
+                              height: Get.height * 0.6,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[50],
+                                  borderRadius: BorderRadius.circular(25)),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 20),
+                                    child: SizedBox(
+                                      width: Get.width * 0.9,
+                                      height: Get.width * 0.6,
+                                      child: Image(
+                                        fit: BoxFit.cover,
+                                        image: AssetImage(
+                                            controller.houseImage[index]),
+                                      ),
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: Get.width * 0.05,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(width: Get.width * 0.04),
-                                    Text("1 BHK house for rent in Gwalior",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: Get.width * 0.045)),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: Get.width * 0.025,
-                                ),
-                                Row(
-                                  children: [
-                                    SizedBox(width: Get.width * 0.04),
-                                    const Text("Post on : 22-09-2022",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                        ))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: Get.width * 0.02,
-                                ),
-                                Row(
-                                  children: [
-                                    SizedBox(width: Get.width * 0.055),
-                                    Column(
-                                      children: [
-                                        Text("₹ 1200 ",
-                                            style: TextStyle(
-                                                fontSize: Get.width * 0.06,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.deepOrange)),
-                                        Text("per month",
-                                            style: TextStyle(
-                                              fontSize: Get.width * 0.035,
-                                              fontWeight: FontWeight.normal,
-                                            )),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.045,
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.3,
-                                      child: CustomElevatedButton(
-                                        padding: 0,
-                                        bgColor: Colors.white,
-                                        textStyle: TextStyle(
-                                            color: Colors.black54,
-                                            fontSize: Get.width * 0.03),
-                                        title: "Schedule visit",
-                                        onPress: () {},
+                                  SizedBox(
+                                    height: Get.width * 0.05,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      SizedBox(width: Get.width * 0.04),
+                                      Text("1 BHK house for rent in Gwalior",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: Get.width * 0.045)),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: Get.width * 0.025,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(width: Get.width * 0.04),
+                                      const Text("Post on : 22-09-2022",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                          ))
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: Get.width * 0.02,
+                                  ),
+                                  Row(
+                                    children: [
+                                      SizedBox(width: Get.width * 0.055),
+                                      Column(
+                                        children: [
+                                          Text("₹ 1200 ",
+                                              style: TextStyle(
+                                                  fontSize: Get.width * 0.06,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.deepOrange)),
+                                          Text("per month",
+                                              style: TextStyle(
+                                                fontSize: Get.width * 0.035,
+                                                fontWeight: FontWeight.normal,
+                                              )),
+                                        ],
                                       ),
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.03,
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.3,
-                                      child: CustomElevatedButton(
-                                        padding: 0,
-                                        textStyle: TextStyle(
-                                            fontSize: Get.width * 0.03,
-                                            color: Colors.white),
-                                        title: "Contact owner",
-                                        onPress: () {},
+                                      SizedBox(
+                                        width: Get.width * 0.045,
                                       ),
-                                    )
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(width: Get.width * 0.035),
-                                    TextButton(
-                                      onPressed: () {},
-                                      child: const Text("Read more..."),
-                                    ),
-                                  ],
-                                )
-                              ],
+                                      SizedBox(
+                                        width: Get.width * 0.3,
+                                        height: Get.width* 0.1,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                                            border: Border.all(color: Colors.grey,
+                                            width: 1.5)
+                                          ),
+                                          child: CustomElevatedButton(
+                                            bgColor: Colors.white,
+                                            padding: 0,
+                                            textStyle: TextStyle(
+                                                color: Colors.black54,
+                                                fontSize: Get.width * 0.03),
+                                            title: "Schedule visit",
+                                            onPress: () {},
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: Get.width * 0.03,
+                                      ),
+                                      SizedBox(
+                                        width: Get.width * 0.3,
+                                        child: CustomElevatedButton(
+                                          padding: 0,
+                                          textStyle: TextStyle(
+                                              fontSize: Get.width * 0.03,
+                                              color: Colors.white),
+                                          title: "Contact owner",
+                                          onPress: () {},
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      SizedBox(width: Get.width * 0.035),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: Text("Read more..."),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                },
-              ),
-              Positioned(
-                top: Get.width * 0.025,
-                left: Get.width * 0.7,
-                child: Container(
-                  height: Get.width * 0.1,
-                  width: Get.width * 0.3,
-                  color: Colors.deepOrange,
-                  child: const Center(
-                    child: Text(
-                      "Verified",
-                      style: TextStyle(color: Colors.white),
+                          ],
+                        ),
+                      ],
                     ),
-                  ),
-                ),
-              )
-            ],
-          ))
+                    Positioned(
+                      top: Get.width * 0.025,
+                      left: Get.width * 0.7,
+                      child: Container(
+                        height: Get.width * 0.1,
+                        width: Get.width * 0.3,
+                        color: Colors.deepOrange,
+                        child: const Center(
+                          child: Text(
+                            "Verified",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    )
+                  ]);
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    )),
+          )
         ],
       ),
     );
